80427c0acd3f2d9bc36253f8a11b79997879a68e
patelkev <77302079+patelkev@users.noreply.github.com>
2023-03-25 20:18:53 -0400

    updated UDPing.c

--- UDPing.c
+++ UDPing.c
@@ -1,249 +1,266 @@
+#include <arpa/inet.h>
+#include <limits.h>
+#include <math.h>
+#include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/socket.h>
-#include <pthread.h>
-#include <netdb.h>
-#include "Practical.h"
-
-struct thread_info {    /* Used as argument to thread_start() */
-    pthread_t thread_id;        /* ID returned by pthread_create() */
-    int       thread_num;       /* Application-defined thread # */
-    char     *server;
-    char *servPort;
-    float interval;
-    int count;
-    int flag;
-    int size;
-    char *echoString;
-    int sock;
-};
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
 
 
+int is_server = 0;
+int packet_count = 10;
+double interval;
+int port = 33333;
+int size = 200;
+int no_print = 0;
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+char* getFinalArg() {
+    extern int optind;
+    extern char **environ;
+
+    int argc = 0;
+    char **argv = environ;
+
+    // count the number of arguments
+    while (argv[argc] != NULL) {
+        argc++;
+    }
+
+    if (argc > 1) {
+        return argv[argc-1];
+    } else {
+        return NULL;
+    }
+}
+
+void run_server() {
+  printf("Starting UDP ping server (port=%d)\n", port);
+
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  struct sockaddr_in serv_addr = {0};
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(port);
+
+  if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("bind");
+    exit(EXIT_FAILURE);
+  }
+
+  struct sockaddr_in cli_addr = {0};
+  socklen_t cli_addr_len = sizeof(cli_addr);
+
+  char buffer[size];
+
+  while (1) {
+    int n = recvfrom(sockfd, buffer, sizeof(buffer), 0,
+                     (struct sockaddr *)&cli_addr, &cli_addr_len);
+    if (n < 0) {
+      perror("recvfrom");
+      exit(EXIT_FAILURE);
+    }
+
+    n = sendto(sockfd, buffer, n, 0, (struct sockaddr *)&cli_addr,
+               cli_addr_len);
+    if (n < 0) {
+      perror("sendto");
+      exit(EXIT_FAILURE);
+    }
+    if (!no_print)
+      printf("Received %d bytes from %s\n", n, inet_ntoa(cli_addr.sin_addr));
+  }
+  close(sockfd);
+}
+
+void *send_ping(void *arg) {
+  //char *ip_addr = (char *) arg;
+
+  int sockfd = *((int *)arg);
+
+  int seq = 1;
+  double rtt;
+  int packets_sent = 0, packets_received = 0;
+  double min_rtt = INT_MAX, max_rtt = 0, avg_rtt = 0;
+
+  struct sockaddr_in serv_addr = {0};
+  serv_addr.sin_family = AF_INET;
+
+
+  // serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serv_addr.sin_addr.s_addr = inet_addr(getFinalArg());
+  serv_addr.sin_port = htons(port);
+
+  struct timespec start_time;
+  clock_gettime(CLOCK_REALTIME, &start_time);
+
+  double totalTime;
+
+  while (packet_count == 0 || seq <= packet_count) {
+    char buffer[size];
+    struct timespec send_time, recv_time;
+    // Prepare ping packet
+    memset(buffer, 0, size);
+    snprintf(buffer, size, "PING %d", seq);
+    // Calculate proper wait time until next ping
+    struct timespec wait_time;
+    wait_time.tv_sec = start_time.tv_sec + (seq - 1) * interval;
+    wait_time.tv_nsec = start_time.tv_nsec;
+    while (wait_time.tv_nsec >= 1000000000L) {
+      wait_time.tv_nsec -= 1000000000L;
+      wait_time.tv_sec++;
+    }
+
+    pthread_mutex_lock(&lock);
+    // Wait for proper time to send next ping
+    pthread_cond_timedwait(&cond, &lock, &wait_time);
+    pthread_mutex_unlock(&lock);
+
+    // Send ping packet
+    clock_gettime(CLOCK_REALTIME, &send_time);
+    int n = sendto(sockfd, buffer, size, 0, (struct sockaddr *)&serv_addr,
+                   sizeof(serv_addr));
+    if (n < 0) {
+      perror("sendto");
+      exit(EXIT_FAILURE);
+    }
+    packets_sent++;
+
+    // Receive pong packet
+    char recv_buffer[size];
+    socklen_t serv_addr_len = sizeof(serv_addr);
+    n = recvfrom(sockfd, recv_buffer, size, 0, (struct sockaddr *)&serv_addr,
+                 &serv_addr_len);
+    if (n < 0) {
+      perror("recvfrom");
+      exit(EXIT_FAILURE);
+    }
+    packets_received++;
+
+    // Get receive time and calculate round-trip time
+    clock_gettime(CLOCK_REALTIME, &recv_time);
+    rtt = (recv_time.tv_sec - send_time.tv_sec) * 1000.0 +
+          (recv_time.tv_nsec - send_time.tv_nsec) / 1000000.0;
+
+    totalTime += rtt;
+
+    // Update min/max/avg RTT
+    if (rtt < min_rtt) {
+      min_rtt = rtt;
+    }
+    if (rtt > max_rtt) {
+      max_rtt = rtt;
+    }
+    avg_rtt += rtt;
+
+    // Print ping-pong message
+    if (!no_print){
+      printf("%d %d %.3f\n", seq, n, rtt);
+    }
+    else {
+      printf("*");
+    }
+    seq++;
+  }
+
+  // Print summary
+  double packet_loss = 100.0 * (packets_sent - packets_received) / packets_sent;
+  double avg_rtt_ms = avg_rtt / packets_received;
+  printf(
+      "\n%d packets transmitted, %d received, %.1f%% packet loss, time %.3f ms\n",
+      packets_sent, packets_received, packet_loss, totalTime);
+  printf("rtt min/avg/max = %.3f/%.3f/%.3f msec\n", min_rtt, avg_rtt_ms,
+         max_rtt);
+  exit(EXIT_SUCCESS);
+}
+
+void run_client() {
+  printf("Starting UDP ping client (server=localhost:%d, packet_count=%d, "
+         "interval=%fms, size=%d)\n",
+         port, packet_count, interval, size);
+
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  pthread_t tid;
+  pthread_create(&tid, NULL, send_ping, &sockfd);
+
+  while (1) {
+    // wait for send_ping to complete packet_count number of pings
+    pthread_mutex_lock(&lock);
+    while (packet_count == 0 || packet_count != -1) {
+      pthread_cond_wait(&cond, &lock);
+    }
+    pthread_mutex_unlock(&lock);
+    pthread_cancel(tid);
+    break;
+  }
+  close(sockfd);
+}
 
 int main(int argc, char *argv[]) {
+  int opt = 0;
+  is_server = 0;
+  char *server_ip_address = NULL;
 
-
-
-  //char *server = argv[1];     // First arg: server address/name
-  //char *echoString = argv[2]; // Second arg: word to echo
-  char *server;
-  int serverReturn;
-  int opt;
-  char *servPort="3333";
-  int printFlag=1;
-  int count=20;
-  int size=300;
-  float interval=1.0;
-  pthread_attr_t attr;
-  void *res;
-
-  struct thread_info *tinfo;
-
-  while((opt = getopt(argc, argv, "S:c:i:p:s:n:")) != -1) { 
-    switch(opt) { 
-
-        //Server mode
-        case 'S': 
-
-            //Next input is the port number
-            serverReturn=runServer(optarg);
-            break;
-
-        //Packet Count    
-        case 'c': 
-
-            //Get next input for packet count
-            tinfo->count=atoi(optarg);
-            fprintf(stderr,"Count: %d\n",count);
-
-            break;
-
-        //Ping interval
-        case 'i': 
-
-            // Use strtok since interval could be a decimal
-            //Get number before decimal
-            printf("Interval: \n");
-            char *str = strtok(optarg, " . "); 
-            int wholeNum=atoi(str);
-
-            //get number after decimal
-            str=strtok(optarg, " . ");
-            int decNum=atoi(str);
-            decNum=decNum/10;
-
-            //THIS IS THE WRONG WAY TO BUILD A DECIMAL
-            tinfo->interval=wholeNum+decNum;
-            fprintf(stderr,"Interval: %f\n",interval);
-
-            break; 
-
-        //port number
-        case 'p': 
-
-            //Next input is the port number
-            servPort=optarg;
-            fprintf(stderr,"Port Number: %s\n",servPort);
-
-            break;
-
-        //size    
-        case 's': 
-
-            //Get next input for size
-            tinfo->size=atoi(optarg);
-            fprintf(stderr,"size: %d\n",size);
-            break;
-
-        //Don't print
-        case 'n': 
-
-            tinfo->flag=0;
-            break;
- 
-        } 
+  while ((opt = getopt(argc, argv, "c:i:p:s:nS")) != -1) {
+    switch (opt) {
+    case 'c':
+      packet_count = atoi(optarg);
+      break;
+    case 'i':
+      interval = atof(optarg);
+      break;
+    case 'p':
+      port = atoi(optarg);
+      break;
+    case 's':
+      size = atoi(optarg);
+      break;
+    case 'n':
+      no_print = 1;
+      break;
+    case 'S':
+      is_server = 1;
+      break;
+    default:
+      fprintf(stderr,
+              "Usage: %s [-c count] [-i interval] [-p port] [-s size] [-n] "
+              "[-S] server_ip_address\n",
+              argv[0]);
+      exit(EXIT_FAILURE);
     }
-    server="babbage1";
-    fprintf(stderr,"Server_IP: %s\n",server);
-
-
-    // Tell the system what kind(s) of address info we want
-  struct addrinfo addrCriteria;                   // Criteria for address match
-  memset(&addrCriteria, 0, sizeof(addrCriteria)); // Zero out structure
-  addrCriteria.ai_family = AF_UNSPEC;             // Any address family
-  // For the following fields, a zero value means "don't care"
-  addrCriteria.ai_socktype = SOCK_DGRAM;          // Only datagram sockets
-  addrCriteria.ai_protocol = IPPROTO_UDP;         // Only UDP protocol
-  fprintf(stderr,"Server_IP: %s\n Server Port: %s\n",server, servPort);
-
-  // Get address(es)
-  struct addrinfo *servAddr; // List of server addresses
-  int rtnVal = getaddrinfo(server, servPort, &addrCriteria, &servAddr);
-  if (rtnVal != 0)
-    DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnVal));
-
-  // Create a datagram/UDP socket
-  fprintf(stderr,"Server_IP: %s\n",server);
-  int sock = socket(servAddr->ai_family, servAddr->ai_socktype,
-      servAddr->ai_protocol); // Socket descriptor for client
-  if (sock < 0)
-    DieWithSystemMessage("socket() failed");
-
-  tinfo = calloc(2, sizeof(struct thread_info));
-
-  int s = pthread_attr_init(&attr);
-
-  for (int i = 0; i < 2; i++) {
-
-  s = pthread_create(&tinfo[i].thread_id, &attr,
-                           &sender, &tinfo[i]);
-
-  s = pthread_create(&tinfo[i].thread_id, &attr,
-                           &receive, &tinfo[i]);
   }
 
-  for (int i = 0; i < 2; i++) {
-
-  s = pthread_join(&tinfo[i].thread_id, &res);
-
-  }
-    
-  free(res);
-  free(tinfo);
-    
-
-
-
-    
-
-
-
-  // Receive a response
-
-  exit(0);
-}
-
-void * sender(void *args) {
-
-  struct thread_info *tinfo = args;
-  tinfo->echoString="AAAAA";
-  size_t echoStringLen = strlen(tinfo->echoString);
-
-  // Third arg (optional): server port/service
-  //char *servPort = (argc == 4) ? argv[3] : "echo";
-
-  // Tell the system what kind(s) of address info we want
-  struct addrinfo addrCriteria;                   // Criteria for address match
-  memset(&addrCriteria, 0, sizeof(addrCriteria)); // Zero out structure
-  addrCriteria.ai_family = AF_UNSPEC;             // Any address family
-  // For the following fields, a zero value means "don't care"
-  addrCriteria.ai_socktype = SOCK_DGRAM;          // Only datagram sockets
-  addrCriteria.ai_protocol = IPPROTO_UDP;         // Only UDP protocol
-
-  // Get address(es)
-  struct addrinfo *servAddr; // List of server addresses
-  int rtnVal = getaddrinfo(tinfo->server, tinfo->servPort, &addrCriteria, &servAddr);
-  if (rtnVal != 0)
-    DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnVal));
-
-  // Create a datagram/UDP socket
-  // int sock; // socket descriptor
-  if ((tinfo->sock = socket(AF_UNSPEC, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
-      perror("socket() failed");
-      exit(-1);
+  if (optind < argc) {
+    server_ip_address = argv[optind];
   }
 
-  // Send the string to the server
-  ssize_t numBytes = sendto(tinfo->sock, tinfo->echoString, echoStringLen, 0,
-      servAddr->ai_addr, servAddr->ai_addrlen);
-  if (numBytes < 0)
-    DieWithSystemMessage("sendto() failed");
-  else if (numBytes != echoStringLen)
-    DieWithUserMessage("sendto() error", "sent unexpected number of bytes");
+  fprintf(stderr, "Count %d\n", packet_count);
+  fprintf(stderr, "Size %d\n", size);
+  fprintf(stderr, "Interval %.3f\n", interval);
+  fprintf(stderr, "Port %d\n", port);
+  fprintf(stderr, "Server_ip %s\n", server_ip_address ? server_ip_address : "");
 
-}
-
-
-
-
-void *receive(void *args) {
-
-  struct thread_info *tinfo = args;
-
-  struct addrinfo addrCriteria;                   // Criteria for address match
-  memset(&addrCriteria, 0, sizeof(addrCriteria)); // Zero out structure
-  addrCriteria.ai_family = AF_UNSPEC;             // Any address family
-  // For the following fields, a zero value means "don't care"
-  addrCriteria.ai_socktype = SOCK_DGRAM;          // Only datagram sockets
-  addrCriteria.ai_protocol = IPPROTO_UDP;         // Only UDP protocol
-
-  // Get address(es)
-  struct addrinfo *servAddr; // List of server addresses
-  int rtnVal = getaddrinfo(tinfo->server, tinfo->servPort, &addrCriteria, &servAddr);
-  if (rtnVal != 0)
-    DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnVal));
-
-
-  struct sockaddr_storage fromAddr; // Source address of server
-  // Set length of from address structure (in-out parameter)
-  socklen_t fromAddrLen = sizeof(fromAddr);
-  char buffer[MAXSTRINGLENGTH + 1]; // I/O buffer
-  ssize_t numBytes = recvfrom(tinfo->sock, buffer, MAXSTRINGLENGTH, 0,
-      (struct sockaddr *) &fromAddr, &fromAddrLen);
-  if (numBytes < 0)
-    DieWithSystemMessage("recvfrom() failed");
-  else if (numBytes != tinfo->size)
-    DieWithUserMessage("recvfrom() error", "received unexpected number of bytes");
-
-  // Verify reception from expected source
-  if (!SockAddrsEqual(servAddr->ai_addr, (struct sockaddr *) &fromAddr))
-    DieWithUserMessage("recvfrom()", "received a packet from unknown source");
-
-  freeaddrinfo(servAddr);
-
-
-  //close(sock);
-
+  if (is_server) {
+    run_server();
+  } else {
+    run_client();
+  }
+  return 0;
 }
